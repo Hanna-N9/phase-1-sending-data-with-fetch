012dafeeadc39f364fdda720c9eac0c038b6e4a6
Hanna Negash <hnegash.n@gmail.com>
2023-09-18 18:03:35 -0700

    Send data with Post request

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+const submitData = (name, email) => {
+  return fetch("http://localhost:3000/users", {
+    method: "Post",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then(res => res.json())
+    .then(object => {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(err => {
+      document.body.textContent = err.message;
+    });
+};
